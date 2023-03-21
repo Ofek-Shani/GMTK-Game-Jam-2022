0f3fdbc6932f3fa7dd0b88165dd9cba9f2b97145
Pei-Yao Hung <peiyaoh@gmail.com>
2023-03-21 15:45:36 -0400

    record prolificId, studyId, sessionId

--- Assets/Scripts/DieController.cs
+++ Assets/Scripts/DieController.cs
@@ -7,12 +7,16 @@ using UnityEngine.Networking;
 struct InteractionReport {
     //Variable declaration
     //Note: I'm explicitly declaring them as public, but they are public by default. You can use private if you choose.
+    public string prolificId;
     public string studyId;
+    public string sessionId;
     public string msg;
 
-    public InteractionReport(string studyId, string msg) {
-        this.studyId = studyId;
+    public InteractionReport(string prolificId, string msg) {
+        this.prolificId = prolificId;
         this.msg = msg;
+        this.studyId = "default";
+        this.sessionId = "default";
     }
 }
 
@@ -116,13 +120,34 @@ public class DieController : MonoBehaviour
         string url = Application.absoluteURL;
         string[] urlSplit = url.Split('?');
         string paramsString = "";
+        string prolificId = "default";
         string studyId = "default";
+        string sessionId = "default";
+
+
+        // example
+
+        /*
+        https://umich.qualtrics.com/jfe/form/SV_4IoSJSkNUKqxOl0?PROLIFIC_PID=63dd5ec01d3544fa8e02d54d&STUDY_ID=63e14c745dea11fca5cff7c8&SESSION_ID=0wa5514d6rl
+
+        https://www-personal.umich.edu/~peiyaoh/LuckingOut/?PROLIFIC_PID=63dd5ec01d3544fa8e02d54d&amp;STUDY_ID=63e14c745dea11fca5cff7c8&amp;SESSION_ID=0wa5514d6rl
+        */
+
 
         if(urlSplit.Length > 1){
             paramsString = urlSplit[1];
             string[] paramSplit = paramsString.Split("&");
             if(paramSplit.Length > 0){
-                studyId = paramSplit[0].Split("=")[1];
+                // PROLIFIC_PID
+                prolificId = paramSplit[0].Split("=")[1];
+            }
+            if(paramSplit.Length > 1){
+                // STUDY_ID
+                studyId = paramSplit[1].Split("=")[1];
+            }
+            if(paramSplit.Length > 1){
+                // SESSION_ID
+                sessionId = paramSplit[2].Split("=")[1];
             }
         }
 
@@ -134,13 +159,23 @@ public class DieController : MonoBehaviour
         yield return request.Send();
         */
 
+        Debug.Log("Query Params - prolificId: " + prolificId);
         Debug.Log("Query Params - studyId: " + studyId);
-        Debug.Log("Query Params - msg: " + msg);
+        Debug.Log("Query Params - sessionId: " + sessionId);
+        Debug.Log("Params - msg: " + msg);
 
 
-        InteractionReport interactionReport = new InteractionReport(studyId, msg);
-        Debug.Log("interactionReport.studyId: " + interactionReport.studyId);
+        InteractionReport interactionReport = new InteractionReport(prolificId, msg);
+        interactionReport.studyId = studyId;
+        interactionReport.sessionId = sessionId;
+
+        Debug.Log("interactionReport.prolificId: " + interactionReport.prolificId);
         Debug.Log("interactionReport.msg: " + interactionReport.msg);
+        Debug.Log("interactionReport.studyId: " + interactionReport.studyId);
+        Debug.Log("interactionReport.sessionId: " + interactionReport.sessionId);
+        
+
+        
 
 
         string jsonString =  JsonUtility.ToJson(interactionReport);
--- service.php
+++ service.php
@@ -1,5 +1,28 @@
 <?php
 
+function write_log($msg)
+{
+    $now = DateTime::createFromFormat('U.u', microtime(true));
+    $timestamp = $now->format("m-d-Y H:i:s.u");
+    error_log("[".$timestamp."] ".$msg."\n", 3, "./tmp/my-errors.log");
+    
+}
+
+function get_ip_address()
+{
+    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
+        if (array_key_exists($key, $_SERVER) === true) {
+            foreach (explode(',', $_SERVER[$key]) as $ip) {
+                if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
+                    return $ip;
+                }
+            }
+        }
+    }
+}
+
+$ip = get_ip_address();
+
 $file = file_get_contents('./.env', true);
 
 $file_split = explode("\n", $file);
@@ -25,7 +48,7 @@ $host = $variable_map["HOST"];
 $port = (int) $variable_map["PORT"];
 
 // json_encode($variable_map) .
-error_log("About to connect!". "\n", 3, "./tmp/my-errors.log");
+//error_log("About to connect!". "\n", 3, "./tmp/my-errors.log");
 
 //echo json_encode($variable_map), "\n";
 
@@ -50,22 +73,22 @@ error_log("After init: "."\n", 3, "./tmp/my-errors.log");
 */
 
 
-error_log("Before connect: "."\t".$entityBody."\n", 3, "./tmp/my-errors.log");
+//error_log("Before connect: "."\t".$entityBody."\n", 3, "./tmp/my-errors.log");
 
 
 //$conn = mysqli_real_connect($link, "localhost", $user, $pswd, $db, $port);
 
-
+write_log("Connect from: ".$ip);
 $conn = mysqli_connect($host, $user, $pswd, $db, $port);
 
-error_log("After connect: "."\t".$entityBody."\n", 3, "./tmp/my-errors.log");
+//error_log("After connect: "."\t".$entityBody."\n", 3, "./tmp/my-errors.log");
 
 if(!$conn){
     // mysqli_connect_error()
-    error_log("Error: ".mysqli_connect_error()."\n", 3, "./tmp/my-errors.log");
+    write_log("Error: ".mysqli_connect_error());
 }
 else if ($conn->connect_errno) {
-    error_log("$conn->connect_error", 3, "./tmp/my-errors.log");
+    write_log("$conn->connect_error");
     echo "Failed to connect to MySQL: " . $conn->connect_error;
     exit();
 }
@@ -79,37 +102,17 @@ else if ($conn->connect_errno) {
 //error_log("After set_charset: "."\n", 3, "./tmp/my-errors.log");
 
 
-function get_ip_address()
-{
-    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
-        if (array_key_exists($key, $_SERVER) === true) {
-            foreach (explode(',', $_SERVER[$key]) as $ip) {
-                if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
-                    return $ip;
-                }
-            }
-        }
-    }
-}
-
-error_log("Before get time: "."\t".$entityBody."\n", 3, "./tmp/my-errors.log");
-
-$now = DateTime::createFromFormat('U.u', microtime(true));
-$now_str = $now->format("m-d-Y H:i:s.u");
-
-error_log("After get time: "."\t".$entityBody."\n", 3, "./tmp/my-errors.log");
 
 
-error_log("Time: "."\t".$now_str."\n", 3, "./tmp/my-errors.log");
+$entityBody = trim(file_get_contents('php://input'));
 
-$entityBody = file_get_contents('php://input');
+//error_log($now_str."\t". "body: ".$entityBody."\n", 3, "./tmp/my-errors.log");
 
-error_log($now_str."\t". "body: ".$entityBody."\n", 3, "./tmp/my-errors.log");
+write_log("body: ".$entityBody);
 
 
-$ip = get_ip_address();
 
-error_log("ip: ".$ip."\n", 3, "./tmp/my-errors.log");
+//error_log("ip: ".$ip."\n", 3, "./tmp/my-errors.log");
 
 //above fine
 
@@ -119,11 +122,14 @@ $input_split = explode("\t",$entityBody);
 error_log("After explode: "."\n", 3, "./tmp/my-errors.log");
 */
 
-error_log("Before json_decode: "."\n", 3, "./tmp/my-errors.log");
+//error_log("Before json_decode: "."\n", 3, "./tmp/my-errors.log");
 $input_json = json_decode($entityBody, true);
+
+/*
 error_log("After json_decode: "."\n", 3, "./tmp/my-errors.log");
 
 error_log("input_json: ".json_encode($input_json)."\n", 3, "./tmp/my-errors.log");
+*/
 
 /*
 $user = mysqli_real_escape_string($conn, $input_split[0]);
@@ -136,30 +142,46 @@ $action = mysqli_real_escape_string($conn, $input_json["msg"]);
 
 
 
-$user = mysqli_real_escape_string($conn, $input_json["studyId"]);
+$user = mysqli_real_escape_string($conn, $input_json["prolificId"]);
 $action =  mysqli_real_escape_string($conn, $input_json["msg"]);
+$studyId = mysqli_real_escape_string($conn, $input_json["studyId"]);
+$sessionId = mysqli_real_escape_string($conn, $input_json["sessionId"]);
 
+/*
 error_log("user: "."$user"."\n", 3, "./tmp/my-errors.log");
 error_log("action: "."$action"."\n", 3, "./tmp/my-errors.log");
+error_log("studyId: "."$studyId"."\n", 3, "./tmp/my-errors.log");
+error_log("sessionId: "."$sessionId"."\n", 3, "./tmp/my-errors.log");
+*/
 
 
-//run the query to search for the username and password the match
+if(strlen($user) < 10 || strlen($action) < 1 || strlen($studyId) < 10 || strlen($sessionId) < 10){
+    write_log("Issue: params invalid!");
+}
+else {
+    //run the query to search for the username and password the match
 
-$query = "INSERT IGNORE INTO log (id, user, action, ip, timestamp) VALUES (NULL, '$user', '$action', '$ip', now())";
+    $query = "INSERT IGNORE INTO log (id, user, action, study_id, session_id, ip, timestamp) VALUES (NULL, '$user', '$action', '$studyId', '$sessionId', '$ip', now())";
 
-//$result = mysql_query($query) or die("Unable to submit session because : " . mysql_error());
-
-
-$result = mysqli_query($conn, $query);
+    //$result = mysql_query($query) or die("Unable to submit session because : " . mysql_error());
 
 
+    $result = mysqli_query($conn, $query);
 
-if (!$result) {
-    die("Unable to a record because : " . mysqli_error($conn));
+
+
+    if (!$result) {
+        $msg = "Unable to a insert a record because : " . mysqli_error($conn);
+        write_log($msg);
+        die($msg);
+    }
+
+
+    $insert_id = mysqli_insert_id($conn);
+    write_log("Successfullly insert a record!");
 }
 
 
-$insert_id = mysqli_insert_id($conn);
 
 $json_result = new stdClass();
 $json_result->message = "ok";
