b72a04d2a6ab328b6edc4994cebf76c00e526008
Pei-Yao Hung <peiyaoh@gmail.com>
2023-03-27 10:57:18 -0400

    update url

--- Assets/Scripts/DieController.cs
+++ Assets/Scripts/DieController.cs
@@ -184,7 +184,7 @@ public class DieController : MonoBehaviour
         Debug.Log("jsonString: " + jsonString);
 
         // using (UnityWebRequest www = UnityWebRequest.Put("http://localhost/LuckingOut/service.php", studyId + "\t"+ msg))
-        using (UnityWebRequest www = UnityWebRequest.Put("http://localhost/LuckingOut/service.php", jsonString))
+        using (UnityWebRequest www = UnityWebRequest.Put("https://d3game.dev.isr.umich.edu/service.php", jsonString))
         {
             // added
             www.SetRequestHeader("Content-Type", "application/json");
